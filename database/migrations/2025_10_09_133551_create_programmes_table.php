ff65c534d526e149599b813d503a077a5d47062a
edenCharis <edenngouanda@gmail.com>
2025-10-09 14:47:59 +0100

    initialisation migrations db

---
+++ database/migrations/2025_10_09_133551_create_programmes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('programmes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('trimestre_id')->constrained('trimestres')->onDelete('cascade');
+            $table->foreignId('etablissement_id')->constrained('etablissements')->onDelete('cascade');
+            $table->foreignId('type_classe_id')->constrained('type_classes')->onDelete('cascade');
+            $table->foreignId('matiere_id')->constrained('matieres')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('programmes');
+    }
+};
